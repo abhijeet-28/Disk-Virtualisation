8bf72fc48196af5a6841879d37f247a013f68e79
Mahdihusain Momin <mahdihusainmomin@gmail.com>
2017-09-15 23:14:02 +0530

    block class tested

--- block.py
+++ block.py
@@ -2,10 +2,18 @@ class block:
 
 	block_size = 100
 	block_mem = None
-
-	def __init__(self):
-		self.block_mem = bytearray(self.block_size)
 	
 	def __init__(self,block_size):
 		self.block_size = block_size
 		self.block_mem = bytearray(self.block_size)
+
+	def put(self,block_info):
+		self.block_mem = bytearray(block_info)
+		self.block_size = len(block_info)
+
+
+if __name__ == "__main__":
+	block_ob = block(100)
+	block_ob.put([1,2,3])
+	print block_ob.block_mem[0]
+	print block_ob.block_size
